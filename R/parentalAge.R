1eeb5dc722c023cc1e70d6f9112fbcec31e17c1c
Tychele Turner <tycheleturner@gmail.com>
2022-10-22 20:12:12 -0500

    [script] adjust the axes

--- R/parentalAge.R
+++ R/parentalAge.R
@@ -1,7 +1,7 @@
 parentalAge <- function(parentalAgeObject) {
     plot(parentalAgeObject[,3], parentalAgeObject[,2], xlab="parent age at birth", ylab="DNV counts", col="steelblue1", pch=19, xlim=c(16,max(parentalAgeObject[,3])+5))
     points(parentalAgeObject[,4], parentalAgeObject[,2], col="orange1", pch=19)
-    legend("bottomright", fill=c("steelblue1", "orange"), legend=c("father's age at birth", "mother's age at birth"), cex=0.6, border=c("steelblue1", "orange1"), box.col=FALSE)
+    legend("topleft", fill=c("steelblue1", "orange"), legend=c("father's age at birth", "mother's age at birth"), cex=0.6, border=c("steelblue1", "orange1"), box.col=FALSE)
 
     fitLinearAll <- lm(dnm_counts~fatherAge+motherAge, data=parentalAgeObject)
     pr.LinearAll <- predict(fitLinearAll)
