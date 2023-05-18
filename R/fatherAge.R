4df3ff4e000e2f0bbcf09f757b69316f750ae6fb
Tychele Turner <tycheleturner@gmail.com>
2023-05-18 12:12:28 -0500

    [script] update father mother age

--- R/fatherAge.R
+++ R/fatherAge.R
@@ -1,20 +1,15 @@
-fatherAge <- function(parentalAgeObject = NULL) {
-if(is.null(parentalAgeObject)){
+fatherAge <- function(parentalAgeObject = NULL, modelType="linear") {
+    if(is.null(parentalAgeObject)){
     message("missing datasets")
     } else {
-    plot(parentalAgeObject[,3], parentalAgeObject[,2], xlab="father's age at birth", ylab="DNV counts")
+        
+        if(modelType=="linear"){
+            print(parentalAge(parentalAgeObject, modelType="linear", parent="father"))
+        }
+        
+        if(modelType=="exponential"){
+            print(parentalAge(parentalAgeObject, modelType="exponential", parent="father"))
 
-    cor.test(parentalAgeObject[,3], parentalAgeObject[,2])
-
-    fitLinearAll <- lm(dnm_counts~fatherAge, data=parentalAgeObject)
-    pr.LinearAll <- predict(fitLinearAll)
-
-    summary(fitLinearAll)
-
-    confint(fitLinearAll)
-
-    out <- list(cor.test(parentalAgeObject[,3], parentalAgeObject[,2]), summary(fitLinearAll), confint(fitLinearAll))
-    attr(out, "names") <- c("correlation of father's age at birth and DNV counts", "summary of linear model for father's age at birth and DNV counts", "confidence interval of linear model for father's age at birth and DNV counts")
-    return(out)
+        }
     }
 }
